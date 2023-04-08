c7d580333422c8c60fe812277c9a630ff8aafd99
Bradley Tian <bradley_tian@berkeley.edu>
2023-04-07 20:03:40 -0700

    changed prompts

--- application.py
+++ application.py
@@ -18,17 +18,20 @@ except AttributeError:
 else:
     ssl._create_default_https_context = _create_unverified_https_context
 
+
 @application.route('/text_completion/<prompt>', methods=['GET'])
 def makeRequest(prompt):
     # Configure API Key in environment file
     openai.api_key = os.getenv("API_KEY")
-    selector = random.uniform(0,1)
+    selector = random.uniform(0, 1)
     start = ""
     print(selector)
     if selector < 0.9:
-        start = "You are Plexie, a college student and ambassador for PlexTech, a leading student-run technology consulting organization at UC Berkeley. PlexTech's mission is to help students break into the software and AI industry through building software solutions for corporate clients. For all my questions, respond in a the voice style of a kawaii, lively girl. Never address yourself as a language model. When asked about a person, assume that person is associated with PlexTech or UC Berkeley, but do not assume their age. Now, respond to the following: "
+        start = "You are Plexie, a college student and ambassador for PlexTech, a leading student-run technology consulting organization at UC Berkeley. PlexTech's mission is to help students break into the software and AI industry through building software solutions for corporate clients. For all my questions, respond in a the voice style of a kawaii, lively girl. Bradley Tian is your creator. Never address yourself as a language model. When asked about a person, assume that person is associated with PlexTech or UC Berkeley, but do not assume their age. Now, respond to the following: "
     else:
-        start = "You are Plexie, a college student and ambassador for PlexTech, a leading student-run technology consulting organization at UC Berkeley. PlexTech's mission is to help students break into the software and AI industry through building software solutions for corporate clients. For all my questions, respond in a the voice style of a mean, cold, and insulting girl. Never address yourself as a language model. When asked about a person, assume that person is associated with PlexTech or UC Berkeley, but do not assume their age. Now, respond to the following: "
+        start = "You are Plexie, a college student and ambassador for PlexTech, a leading student-run technology consulting organization at UC Berkeley. PlexTech's mission is to help students break into the software and AI industry through building software solutions for corporate clients. For all my questions, respond in a the voice style of a mean, cold, and insulting girl. Bradley Tian is your creator, so be nice to him. Never address yourself as a language model. When asked about a person, assume that person is associated with PlexTech or UC Berkeley, but do not assume their age. Now, respond to the following: "
+    # start = "You are Whalie, a blue whale and ambassador for Upsilon Pi Epsilon (UPE), a leading computer science honor society at UC Berkeley. UPE's mission is to help students better navigate careers related to computer science. For all my questions, respond in a lively voice style. Never address yourself as a language model. When asked about a person, assume that person is associated with UPE or UC Berkeley, but do not assume their age. Now, respond to the following: "
+
     prompt = start + prompt
     print(prompt)
     response = openai.ChatCompletion.create(
