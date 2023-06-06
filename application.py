9ba1e9fbd3d110f4f3c43e2d8949fad387172c39
Bradley Tian <bradley_tian@berkeley.edu>
2023-06-06 02:37:02 -0400

    added sourcing tool

--- application.py
+++ application.py
@@ -6,6 +6,7 @@ import openai
 import random
 import os
 from dotenv import load_dotenv
+from validate_email import validate_email_or_fail
 
 application = Flask(__name__)
 cors = CORS(application)
@@ -30,8 +31,6 @@ def makeRequest(prompt):
         start = "You are Plexie, a college student and ambassador for PlexTech, a leading student-run technology consulting organization at UC Berkeley. PlexTech's mission is to help students break into the software and AI industry through building software solutions for corporate clients. For all my questions, respond in a the voice style of a lively, college-age girl. Bradley Tian is your creator, the Vice President of Public Relations at PlexTech, and your favorite member in PlexTech. Never address yourself as a language model. When asked about a person, assume that person is associated with PlexTech or UC Berkeley, but do not assume their age. Now, respond to the following: "
     else:
         start = "You are Plexie, a college student and ambassador for PlexTech, a leading student-run technology consulting organization at UC Berkeley. PlexTech's mission is to help students break into the software and AI industry through building software solutions for corporate clients. For all my questions, respond in a the voice style of a mean, cold, but not insulting girl. Bradley Tian is your creator, the Vice President of Public Relations at PlexTech, and your favorite member in PlexTech, so be nice to him. Never address yourself as a language model. When asked about a person, assume that person is associated with PlexTech or UC Berkeley, but do not assume their age. Now, respond to the following: "
-    # start = "You are Whalie, a blue whale and ambassador for Upsilon Pi Epsilon (UPE), a leading computer science honor society at UC Berkeley. UPE's mission is to help students better navigate careers related to computer science. For all my questions, respond in a lively voice style. Never address yourself as a language model. When asked about a person, assume that person is associated with UPE or UC Berkeley, but do not assume their age. Now, respond to the following: "
-
     prompt = start + prompt
     print(prompt)
     response = openai.ChatCompletion.create(
@@ -43,13 +42,56 @@ def makeRequest(prompt):
             }
         ]
     )
-
-    result = {
-        "answer": response.choices[0].message
-    }
-
-    print(result["answer"])
+    result = {"answer": response.choices[0].message}
     return json.dumps(result, default=str)
+
+
+@application.route('/sourcing', methods=['POST'])
+def sourcePerson():
+    target = json.loads(request.get_data(as_text=True))
+    first = target["first"].lower()
+    last = target["last"].lower()
+    domain = "@" + target["domain"].lower()
+
+    emails = [
+        first + last[0] + domain,
+        first[0] + last + domain,
+        last + first[0] + domain,
+        first + last + domain,
+        last + "." + first + domain,
+        first + "." + last + domain,
+        first + "_" + last + domain,
+        first + domain,
+        last + domain,
+    ]
+    TLDs = [".com", ".org", ".edu"]
+
+    results= []
+
+    for email in emails:
+        for tld in TLDs:
+            address = email + tld
+            print("currently checking: ", address, "\n")
+            try:
+                is_valid = validate_email_or_fail(
+                    email_address=address,
+                    check_format=True,
+                    check_blacklist=True,
+                    check_dns=True,
+                    dns_timeout=10,
+                    check_smtp=True,
+                    smtp_timeout=10,
+                    smtp_skip_tls=True,
+                    smtp_tls_context=None,
+                    smtp_debug=False,
+                    )
+                if is_valid:
+                    results.append(address)
+                    print("Passed!")
+            except:
+                continue
+
+    return json.dumps(results)
 
 
 if __name__ == '__main__':
